041c86ef0ab36d914a0cb34d78b17bfe01150332
ppatel31102003 <ppatel31102003@gmail.com>
2026-02-02 23:27:47 +0530

    Develop Camera section

--- src/App.js
+++ src/App.js
@@ -4,13 +4,22 @@ import './App.css';
 // Import hình ảnh - bạn sẽ thêm các file này vào thư mục src/
 
 import News from './components/News'
+import TrafficCameraMonitor from './components/Trafficcameramonitor';
 
 function App() {
  
 
   return (
-    <div className="app-container">
-      <News/>
+
+
+        <div className="app-layout">
+      <div className="left-panel">
+        <News />
+      </div>
+
+      <div className="right-panel">
+        <TrafficCameraMonitor />
+      </div>
     </div>
   );
 }
